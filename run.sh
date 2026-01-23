5d6200e84a64f62314c73750689b9ecc6c456f45
Concha Labra <clabra@gmail.com>
2026-01-23 16:11:58 +0000

    d2: arch diagrams updated in  v0.0.31-gtransformer-pca branch

--- run.sh
+++ run.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-# Simple launcher for run_repro_experiment.py
+# Simple launcher for run_benchmarks_paper.py
 # Usage: ./run.sh [arguments...]
 
 cd /workspaces/pykt-toolkit
 source /home/vscode/.pykt-env/bin/activate
-python examples/run_repro_experiment.py "$@"
+
+nohup python examples/run_benchmarks_paper.py  --mode training --model gtransformer --dataset assist2009  --gpus 1,2,3,4,5 "$@" > experiments/run_benchmarks_paper.log 2>&1 &
+
+# Evaluation
+#python examples/run_benchmarks_paper.py --mode evaluation  --model gtransformer --dataset assist2009 "$@"
+
+#Results
+#python examples/run_benchmarks_paper.py  --mode results  --model gtransformer --dataset assist2009 "$@"
